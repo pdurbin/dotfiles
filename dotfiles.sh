918e7a680a32c8cad5037a3bc3bfa5559b1701bf
Philip Durbin <philipdurbin@gmail.com>
2012-06-06 21:24:15 -0400

    added --list

--- dotfiles.sh
+++ dotfiles.sh
@@ -5,9 +5,13 @@ case "$1" in
   -m|--make-links)
     $CMD
     ;;
+  -l|--list)
+    find ~ -maxdepth 1 -type l | sort | xargs file
+    ;;
   *)
     $CMD --dry-run
     echo
     echo "Run with -m or --make-links to actually make the links"
+    echo "Run with -l or --list to show symlinks in $HOME"
     ;;
 esac
